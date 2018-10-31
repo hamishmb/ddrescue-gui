d2f57ffa44de0ac98156f80a87716a69aba302c5
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-10-31 15:27:49 +0000

    Add GetDevInfo version to about box

--- DDRescue_GUI.py
+++ DDRescue_GUI.py
@@ -1368,7 +1368,8 @@ class MainWindow(wx.Frame): #pylint: disable=too-many-instance-attributes,too-ma
         aboutbox.Description = "GUI frontend for GNU ddrescue\n\nPython version " \
                                + sys.version.split()[0] \
                                + "\nwxPython version " + wx.version() \
-                               + "\nGNU ddrescue version " + SETTINGS["DDRescueVersion"]
+                               + "\nGNU ddrescue version " + SETTINGS["DDRescueVersion"] \
+                               + "\nGetDevInfo version " + getdevinfo.getdevinfo.VERSION
 
         aboutbox.WebSite = ("http://www.hamishmb.com", "My Website")
         aboutbox.Developers = ["Hamish McIntyre-Bhatty", "Minnie McIntyre-Bhatty (GUI Design)"]
