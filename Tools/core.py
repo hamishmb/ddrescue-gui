493e25b33a16039aaf7e703e08bcf149f3d312a8
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-08-18 11:50:28 +0100

    Fix unmounting output files on mac OS

--- Tools/core.py
+++ Tools/core.py
@@ -796,7 +796,7 @@ def is_mounted(partition, mount_point=None):
         #LINUX fix: Accept any mountpoint when called with just one argument.
         for line in mount_info.split("\n"):
             if line and line.split(" on ")[0] == partition \
-                or line.split(" on ")[1] == partition \
+                or line.split(" ")[2] == partition \
                 or line.split(" on ")[1].split(" type ")[0] == partition:
 
                 disk_is_mounted = True
