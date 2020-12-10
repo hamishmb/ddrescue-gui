46497c49dc0c2d3497bf2592b6e61bc9a7fd6b0b
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-12-10 15:47:00 +0000

    Update macos wxpython setup script for wxPython 4.1.x

--- other/macos-scripts/setup_wxpython.sh
+++ other/macos-scripts/setup_wxpython.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd ../../dist/DDRescue_GUI.app/Contents/Resources/lib/python3.8/lib-dynload/wx
-ln -s ../../../../../Frameworks/libwx_baseu-3.1.4.0.0.dylib ./
-ln -s ../../../../../Frameworks/libwx_baseu_net-3.1.4.0.0.dylib ./
-ln -s ../../../../../Frameworks/libwx_osx_cocoau_adv-3.1.4.0.0.dylib ./
-ln -s ../../../../../Frameworks/libwx_osx_cocoau_core-3.1.4.0.0.dylib ./
-ln -s ../../../../../Frameworks/libwx_osx_cocoau_stc-3.1.4.0.0.dylib ./
+cd ../../dist/DDRescue_GUI.app/Contents/Resources/lib/python3.9/lib-dynload/wx
+ln -s ../../../../../Frameworks/libwx_baseu-3.1.5.0.0.dylib ./
+ln -s ../../../../../Frameworks/libwx_baseu_net-3.1.5.0.0.dylib ./
+ln -s ../../../../../Frameworks/libwx_osx_cocoau_adv-3.1.5.0.0.dylib ./
+ln -s ../../../../../Frameworks/libwx_osx_cocoau_core-3.1.5.0.0.dylib ./
+ln -s ../../../../../Frameworks/libwx_osx_cocoau_stc-3.1.5.0.0.dylib ./
 cd -
